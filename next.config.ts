2f1c310be1556e3cf28a4d95c26ee26f55f51820
web-am <andrea.dev00@gmail.com>
2025-12-26 17:37:32 +0100

    update 104

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  eslint: {
+    // Ignora gli errori ESLint durante la build
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
